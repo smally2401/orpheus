386eff8d47c6dba661826dfc95a7af9eec4dafa8
iris <ybecerra07@gmail.com>
2026-07-14 20:26:57 +0200

    fixed config example 02

--- config_examples/02_daytime.lua
+++ config_examples/02_daytime.lua
@@ -3,18 +3,18 @@ local hour = t.hour
 
 if hour >= 22 or hour <= 7 then
     -- nighttime
-    sidebar_bg = "#e6e8f5"
-    now_playing_bar_bg = "f4f5fb"
-    library_view_bg = "#d6d4eb"
-    album_view_bg = "#d6d4eb"
-    playlists_view_bg = "#d6d4eb"
-    open_playlist_view_bg = "#d6d4eb"
-else
-    -- daytime
     sidebar_bg = "#0e101d"
     now_playing_bar_bg = "#0a0a0f"
     library_view_bg = "#1f1d2f"
     album_view_bg = "#1f1d2f"
     playlists_view_bg = "#1f1d2f"
     open_playlist_view_bg = "#1f1d2f"
+else
+    -- daytime
+    sidebar_bg = "#e6e8f5"
+    now_playing_bar_bg = "f4f5fb"
+    library_view_bg = "#d6d4eb"
+    album_view_bg = "#d6d4eb"
+    playlists_view_bg = "#d6d4eb"
+    open_playlist_view_bg = "#d6d4eb"
 end
