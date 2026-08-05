1f24d4be3c6d07c429c21bb2a2b38894059c1251
iris <ybecerra07@gmail.com>
2026-08-05 14:05:20 +0200

    started android app

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -5,3 +5,10 @@ pub(crate) mod mpris;
 pub mod player_bridge;
 pub(crate) mod state;
 pub mod utils;
+
+uniffi::setup_scaffolding!();
+
+#[uniffi::export]
+pub fn android_text() -> String {
+    String::from("Hello, World!")
+}
